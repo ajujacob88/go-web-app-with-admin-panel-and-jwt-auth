8daccf381a6ac4c2e110f00802fb43159cc06060
Aju Jacob <ajujacob88@gmail.com>
2023-04-21 18:25:24 +0530

    connected with db

---
+++ initializers/syncDatabase.go
@@ -0,0 +1,8 @@
+package initializers
+
+import "github.com/ajujacob88/go-web-app-with-admin-panel-and-jwt-auth/models"
+
+func SyncDatabase() {
+	DB.AutoMigrate(&models.User{})
+
+}
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/ajujacob88/go-web-app-with-admin-panel-and-jwt-auth/initializers"
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
