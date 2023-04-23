631faa9317d0326c0d0653455e6db5bf51e3ca85
Aju Jacob <ajujacob88@gmail.com>
2023-04-23 20:18:21 +0530

    user operations panel completed

--- middleware/requireAuth.go
+++ middleware/requireAuth.go
@@ -13,6 +13,63 @@ import (
 )
 
 func RequireAuth(c *gin.Context) {
+	// Get the cookie off request
+	tokenString, err := c.Cookie("Authorization")
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	// Decode / validate it
+
+	// Parse takes the token string and a function for looking up the key. The latter is especially
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+
+		// Check the expiration and not before time
+		expTime := int64(claims["exp"].(float64))
+		nbfTime := int64(claims["nbf"].(float64))
+		if time.Now().Unix() > expTime || time.Now().Unix() < nbfTime {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		// Find the user with token sub
+		var user models.User
+		initializers.DB.First(&user, claims["sub"])
+
+		if user.ID == 0 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+			return
+		}
+
+		// Attach user to request context
+		c.Set("user", user)
+
+		// Continue with the request
+		c.Next()
+
+	} else {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Unauthorized"})
+		return
+	}
+}
+
+/*
+func RequireAuth(c *gin.Context) {
 	fmt.Println("In middleware")
 
 	// Get the cookie off request
@@ -64,3 +121,5 @@ func RequireAuth(c *gin.Context) {
 	}
 
 }
+
+*/
